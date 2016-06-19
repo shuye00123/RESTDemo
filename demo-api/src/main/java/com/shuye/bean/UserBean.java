0fcce24efcea44a59ef5eb9fdc3816a57ca0be7e
shuye <shuye00123@qq.com>
2016-06-19 17:57:15 +0800

    pojo

---
+++ demo-api/src/main/java/com/shuye/bean/UserBean.java
@@ -0,0 +1,19 @@
+package com.shuye.bean;
+
+public class UserBean {
+
+	private String token;
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	private String username;
+}
---
+++ demo-api/src/main/java/com/shuye/result/UserResult.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.shuye.result;
+
+/**
+ * @author shuye
+ *
+ */
+public class UserResult {
+
+	private String username;
+	private String password;
+	private String createdTime;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCreatedTime() {
+		return createdTime;
+	}
+	public void setCreatedTime(String createdTime) {
+		this.createdTime = createdTime;
+	}
+}
